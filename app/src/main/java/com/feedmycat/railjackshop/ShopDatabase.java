acf155e1d677f2e4a1f06cc25db98f12daa830d7
feedmycat <konstantangelou@gmail.com>
2020-05-23 21:05:23 +0300

    Add data to the database when it is created

--- app/src/main/java/com/feedmycat/railjackshop/ShopDatabase.java
+++ app/src/main/java/com/feedmycat/railjackshop/ShopDatabase.java
@@ -1,9 +1,12 @@
 package com.feedmycat.railjackshop;
 
 import android.content.Context;
+import android.os.AsyncTask;
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 @Database(entities = {Customer.class, Administrator.class}, version = 1)
 public abstract class ShopDatabase extends RoomDatabase {
@@ -19,8 +22,38 @@ public abstract class ShopDatabase extends RoomDatabase {
       instance = Room.databaseBuilder(context.getApplicationContext(),
           ShopDatabase.class, "shop_database")
           .fallbackToDestructiveMigration()
+          .addCallback(roomCallback)
           .build();
     }
     return instance;
+  }
+
+  private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback() {
+    //Fills the database when it is created
+    @Override
+    public void onCreate(@NonNull SupportSQLiteDatabase db) {
+      super.onCreate(db);
+      new PopulateDbAsyncTask(instance).execute();
+    }
+  };
+
+  //Async task for populating the database on the background thread
+  private static class PopulateDbAsyncTask extends AsyncTask<Void, Void, Void> {
+    private CustomerDao customerDao;
+    private AdministratorDao administratorDao;
+
+    public PopulateDbAsyncTask(ShopDatabase db) {
+      this.customerDao = db.customerDao();
+      this.administratorDao = db.administratorDao();
+    }
+
+    @Override
+    protected Void doInBackground(Void... voids) {
+      customerDao.insert(new Customer("cus1", "1234", 100));
+      customerDao.insert(new Customer("cus2", "1234", 200));
+      customerDao.insert(new Customer("cus3", "1234", 50));
+      administratorDao.insert(new Administrator("admin1", "admin"));
+      return null;
+    }
   }
 }
