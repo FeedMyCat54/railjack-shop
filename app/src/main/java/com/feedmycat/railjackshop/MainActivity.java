feac8f98964df6e0a691a5f69b97a0347e77af05
feedmycat <konstantangelou@gmail.com>
2020-05-23 18:51:01 +0300

    Add empty project

---
+++ app/src/main/java/com/feedmycat/railjackshop/MainActivity.java
@@ -0,0 +1,13 @@
+package com.feedmycat.railjackshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
