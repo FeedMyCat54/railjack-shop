a5001bc9bb6ec23c06899b6576223fc8b807b73a
feedmycat <konstantangelou@gmail.com>
2020-05-24 00:27:28 +0300

    Add customer observer observer

--- app/src/main/java/com/feedmycat/railjackshop/MainActivity.java
+++ app/src/main/java/com/feedmycat/railjackshop/MainActivity.java
@@ -1,13 +1,29 @@
 package com.feedmycat.railjackshop;
 
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+  private CustomerViewModel customerViewModel;
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    //References to the ViewModels
+    customerViewModel = new ViewModelProvider(this).get(CustomerViewModel.class);
+
+    //Customer observer
+    customerViewModel.getAllCustomers().observe(this, new Observer<List<Customer>>() {
+      @Override
+      public void onChanged(List<Customer> customers) {
+        //Update RecyclerView
+        Toast.makeText(MainActivity.this, "onChanged", Toast.LENGTH_SHORT).show();
+      }
+    });
   }
 }
