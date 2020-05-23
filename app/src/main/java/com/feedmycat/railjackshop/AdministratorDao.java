dfc1d26f58d3ba5ae7c32063562d74712128510c
feedmycat <konstantangelou@gmail.com>
2020-05-23 20:00:22 +0300

    Add customer and administrator Daos

---
+++ app/src/main/java/com/feedmycat/railjackshop/AdministratorDao.java
@@ -0,0 +1,31 @@
+package com.feedmycat.railjackshop;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+import java.util.List;
+
+@Dao
+public interface AdministratorDao {
+
+  @Insert
+  void insert(Administrator administrator);
+
+  @Update
+  void update(Administrator administrator);
+
+  @Delete
+  void delete(Administrator administrator);
+
+  @Query("DELETE FROM administrator_table")
+  void deleteAllAdministrators();
+
+  @Query("SELECT * FROM administrator_table ORDER BY id ASC")
+  LiveData<List<Administrator>> getAllAdministrators();
+
+  @Query("SELECT * FROM administrator_table WHERE id = :id")
+  Administrator getAdministratorById(long id);
+}
---
+++ app/src/main/java/com/feedmycat/railjackshop/CustomerDao.java
@@ -0,0 +1,31 @@
+package com.feedmycat.railjackshop;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+import java.util.List;
+
+@Dao
+public interface CustomerDao {
+
+  @Insert
+  void insert(Customer customer);
+
+  @Update
+  void update(Customer customer);
+
+  @Delete
+  void delete(Customer customer);
+
+  @Query("DELETE FROM customer_table")
+  void deleteAllCustomers();
+
+  @Query("SELECT * FROM customer_table ORDER BY id ASC")
+  LiveData<List<Customer>> getAllCustomers();
+
+  @Query("SELECT * FROM customer_table WHERE id = :id")
+  Customer getCustomerById(long id);
+}
