b39895a1dbed0122a4299d7bf4e6f624e8963ac5
feedmycat <konstantangelou@gmail.com>
2020-05-24 13:36:33 +0300

    Add Administrator observer

--- app/src/main/java/com/feedmycat/railjackshop/MainActivity.java
+++ app/src/main/java/com/feedmycat/railjackshop/MainActivity.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
   private CustomerViewModel customerViewModel;
+  private AdministratorViewModel administratorViewModel;
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -16,13 +17,23 @@ public class MainActivity extends AppCompatActivity {
 
     //References to the ViewModels
     customerViewModel = new ViewModelProvider.AndroidViewModelFactory(getApplication()).create(CustomerViewModel.class);
+    administratorViewModel = new ViewModelProvider.AndroidViewModelFactory(getApplication()).create(AdministratorViewModel.class);
 
     //Customer observer
     customerViewModel.getAllCustomers().observe(this, new Observer<List<Customer>>() {
       @Override
       public void onChanged(List<Customer> customers) {
         //Update RecyclerView
-        Toast.makeText(MainActivity.this, "onChanged", Toast.LENGTH_SHORT).show();
+        Toast.makeText(MainActivity.this, "Customer", Toast.LENGTH_SHORT).show();
+      }
+    });
+
+    //Administrator observer
+    administratorViewModel.getAllCustomers().observe(this, new Observer<List<Administrator>>() {
+      @Override
+      public void onChanged(List<Administrator> administrators) {
+        //Update RecyclerView
+        Toast.makeText(MainActivity.this, "Administrator", Toast.LENGTH_SHORT).show();
       }
     });
   }
