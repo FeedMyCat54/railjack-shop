a3e7ea70ab13c2f80fa5b3da7f2da9750a72a15a
feedmycat <konstantangelou@gmail.com>
2020-05-24 13:31:28 +0300

    Add Administrator viewModel

---
+++ app/src/main/java/com/feedmycat/railjackshop/AdministratorViewModel.java
@@ -0,0 +1,39 @@
+package com.feedmycat.railjackshop;
+
+import android.app.Application;
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import java.util.List;
+
+public class AdministratorViewModel extends AndroidViewModel {
+  private AdministratorRepository repository;
+  private LiveData<List<Administrator>> allAdministrators;
+
+  public AdministratorViewModel(@NonNull Application application) {
+    super(application);
+    repository = new AdministratorRepository(application);
+    allAdministrators = repository.getAllAdministrators();
+  }
+
+  //Methods to pass data from the ViewModel to the repository
+  public void insert(Administrator administrator) {
+    repository.insert(administrator);
+  }
+
+  public void update(Administrator administrator) {
+    repository.update(administrator);
+  }
+
+  public void delete(Administrator administrator) {
+    repository.delete(administrator);
+  }
+
+  public void deleteAllCustomers() {
+    repository.deleteAllAdministrators();
+  }
+
+  public LiveData<List<Administrator>> getAllCustomers() {
+    return allAdministrators;
+  }
+}
