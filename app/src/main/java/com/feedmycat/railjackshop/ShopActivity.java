e6a617d13fa8020f0836181523aaa5dd0f0147e8
FeedMyCat54 <konstantangelou@gmail.com>
2020-05-28 01:27:45 +0300

    Implement shop recyclerView

--- app/src/main/java/com/feedmycat/railjackshop/ShopActivity.java
+++ app/src/main/java/com/feedmycat/railjackshop/ShopActivity.java
@@ -1,15 +1,37 @@
 package com.feedmycat.railjackshop;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.List;
+
 public class ShopActivity extends AppCompatActivity {
+    private ProductViewModel productViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
+
+        RecyclerView recyclerView = findViewById(R.id.rv_shop);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        final ProductAdapter adapter = new ProductAdapter();
+        recyclerView.setAdapter(adapter);
+
+        productViewModel = new ViewModelProvider.AndroidViewModelFactory(getApplication()).create(ProductViewModel.class);
+        productViewModel.getAllProducts().observe(this, new Observer<List<Product>>() {
+            @Override
+            public void onChanged(List<Product> products) {
+                adapter.setProducts(products);
+            }
+        });
     }
 }
