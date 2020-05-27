1f7efedc534a3589d91470915e05fe71a0426a57
FeedMyCat54 <konstantangelou@gmail.com>
2020-05-27 20:10:46 +0300

    Add shop item layout

--- app/src/main/java/com/feedmycat/railjackshop/ShopActivity.java
+++ app/src/main/java/com/feedmycat/railjackshop/ShopActivity.java
@@ -6,16 +6,10 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class ShopActivity extends AppCompatActivity {
-    TextView tvHello;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
-
-        tvHello = findViewById(R.id.tv_hello);
-        int userID = getIntent().getExtras().getInt("EXTRA_CUSTOMER_ID");
-
-        tvHello.setText(String.valueOf(userID));
     }
 }
