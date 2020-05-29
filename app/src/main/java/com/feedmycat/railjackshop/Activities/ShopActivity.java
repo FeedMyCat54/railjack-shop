c6cc9d87a504a56a31a31f50f1d67bb6bcf2d28f
FeedMyCat54 <konstantangelou@gmail.com>
2020-05-30 01:41:50 +0300

    Add Drawer and redesign ShopActivity to use fragments

--- app/src/main/java/com/feedmycat/railjackshop/Activities/ShopActivity.java
+++ app/src/main/java/com/feedmycat/railjackshop/Activities/ShopActivity.java
@@ -1,11 +1,29 @@
 package com.feedmycat.railjackshop.Activities;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 import com.feedmycat.railjackshop.Fragments.ShopFragment;
 import com.feedmycat.railjackshop.R;
+import com.google.android.material.navigation.NavigationView;
 
-public class ShopActivity extends AppCompatActivity {
+public class ShopActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    Toolbar toolbar;
+    NavigationView navigationView;
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
+
     private int customerId;
 
     @Override
@@ -13,9 +31,40 @@ public class ShopActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
 
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        drawerLayout = findViewById(R.id.drawer);
+        navigationView = findViewById(R.id.navigationView);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+        actionBarDrawerToggle.syncState();
+
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+
         customerId = getIntent().getIntExtra("EXTRA_CUSTOMER_ID", 0);
 
-        ShopFragment fragment = ShopFragment.newInstance(customerId);
-        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+        fragmentTransaction.add(R.id.container_fragment, ShopFragment.newInstance(customerId));
+        fragmentTransaction.commit();
+
+//        ShopFragment fragment = ShopFragment.newInstance(customerId);
+//        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        drawerLayout.closeDrawer(GravityCompat.START);
+        if (item.getItemId() == R.id.shop) {
+            fragmentManager = getSupportFragmentManager();
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.container_fragment, ShopFragment.newInstance(customerId));
+            fragmentTransaction.commit();
+        }
+        return true;
     }
 }
