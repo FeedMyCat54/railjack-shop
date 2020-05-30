4d61cda4fda498bb921a0f0976ecd8c7ce40a696
feedmycat <konstantangelou@gmail.com>
2020-05-30 17:58:57 +0300

    Add CartItem viewModel

---
+++ app/src/main/java/com/feedmycat/railjackshop/ViewModels/CartItemViewModel.java
@@ -0,0 +1,35 @@
+package com.feedmycat.railjackshop.ViewModels;
+
+import android.app.Application;
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import com.feedmycat.railjackshop.Entities.CartItem;
+import com.feedmycat.railjackshop.Repositories.CartItemRepository;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+public class CartItemViewModel extends AndroidViewModel {
+
+  private CartItemRepository repository;
+
+  public CartItemViewModel(@NonNull Application application) {
+    super(application);
+    repository = new CartItemRepository(application);
+  }
+
+  public void insert(CartItem cartItem) {
+    repository.insert(cartItem);
+  }
+
+  public void update(CartItem cartItem) {
+    repository.update(cartItem);
+  }
+
+  public void delete(CartItem cartItem) {
+    repository.delete(cartItem);
+  }
+
+  public List<CartItem> findItemsForCart(int id) throws ExecutionException, InterruptedException {
+    return repository.findItemsForCart(id);
+  }
+}
